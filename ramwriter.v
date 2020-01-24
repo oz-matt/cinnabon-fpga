835b3b7c7815c00ca8f78bfd8e3a3365fed097dc
ozoralabs <matt.morra@ozoralabs.com>
2020-01-24 17:42:14 -0500

    100sps thru-put for testing

--- ramwriter.v
+++ ramwriter.v
@@ -17,13 +17,13 @@ reg[13:0] r_address = 0;
 reg[7:0] r_byteen = 8'hFF;
 reg r_wbit = 0;
 
-reg[3:0] clk_ctr = 0;
+reg[31:0] clk_ctr = 0;
 
 
 parameter INIT_STATE = 3'b000;
 parameter START_WRITE = 3'b001;
 parameter END_WRITE = 3'b010;
-parameter NEXT_ADDY_AND_DATA = 3'b011;
+parameter WAIT_STATE = 3'b011;
 parameter STOP_ALL = 3'b100;
 
 reg[3:0] current_state = INIT_STATE;
@@ -63,9 +63,16 @@ begin
 		if(r_address[13] == 1)
 		  current_state <= STOP_ALL;
 		else
-		  current_state <= START_WRITE;
+		  current_state <= WAIT_STATE;
 	 end
 	 
+	 WAIT_STATE:
+	 begin
+	   if(clk_ctr >= 499998) //Start with 100samples/sec for testing
+		  current_state <= START_WRITE;
+		else
+		  clk_ctr <= clk_ctr + 1;
+	 end
 	 
 	 
 	 STOP_ALL:
