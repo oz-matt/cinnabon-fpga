808b4c374f14b58da27fb7675882a25583283db7
ozoralabs <matt.morra@ozoralabs.com>
2020-02-23 18:59:36 -0500

    changed compile script for resiming

--- simulation/modelsim/cinnabon_fpga_iputf_input/synopsys/vcs/vcs_setup.sh
+++
@@ -1,3 +0,0 @@
-cp -f D:/cinnabon_fpga/cores/nco/nco/simulation/submodules/nco_nco_ii_0_sin.hex ./
-cp -f D:/cinnabon_fpga/cores/nco/nco/simulation/submodules/nco_nco_ii_0_cos.hex ./
-cp -f D:/cinnabon_fpga/cinnabon_fpga_qsys/simulation/submodules/cinnabon_fpga_qsys_onchip_memory.hex ./
